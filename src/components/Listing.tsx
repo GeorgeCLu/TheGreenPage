75ff60e8cc2ec477b7643fcf57ee6206d1537e55
George <georgelu333@hotmail.com>
2021-11-15 12:42:05 +1300

    modified Listing

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -1,43 +1,43 @@
 /* eslint-disable react/no-unescaped-entities */
 /* eslint-disable max-len */
 /* eslint-disable react/prop-types */
-import React, { useState } from 'react';
-import { useQuery } from '@apollo/client';
-import Listings from './Listings';
-import { ALL_LISTINGS } from '../queries';
+import React, { useState } from 'react'
+import { useQuery } from '@apollo/client'
+import Listings from './Listings'
+import { ALL_LISTINGS } from '../queries'
 import ListingProp from '../common/listing';
-import NotifyMessage from './NotifyMessage';
+import NotifyMessage from './NotifyMessage'
 
 const Listing = () => {
-  const [errorMessage, setErrorMessage] = useState<string|null>(null);
-  const result = useQuery(ALL_LISTINGS);
+  const [errorMessage, setErrorMessage] = useState<string|null>(null)
+  const result = useQuery(ALL_LISTINGS)
 
-  if (result.loading) {
-    return <div>loading...</div>;
+  if (result.loading)  {
+    return <div>loading...</div>
   }
 
   // eslint-disable-next-line no-unused-vars
   const notify = (message: string|null) => {
-    setErrorMessage(message);
+    setErrorMessage(message)
     setTimeout(() => {
-      setErrorMessage(null);
-    }, 5000);
-  };
+      setErrorMessage(null)
+    }, 5000)
+  }
 
-  if (result.data) {
+  if (result.data){
     const listings = result.data.allListings as ListingProp[];
     return (
       <div>
         <NotifyMessage errorMessage={errorMessage} />
         <Listings listings={listings} />
       </div>
-    );
-  }
-  return (
+    )
+   }
+   return (
     <div>
       <NotifyMessage errorMessage={errorMessage} />
     </div>
-  );
+   )
 };
 
 export default Listing;
