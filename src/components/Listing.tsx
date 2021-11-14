c9e13d7199e6b0287c9be6bab748d9289cec92ad
George <georgelu333@hotmail.com>
2021-11-15 11:05:08 +1300

    changed Listing

--- src/components/Listing.tsx
+++ src/components/Listing.tsx
@@ -16,7 +16,6 @@ const Listing = () => {
     return <div>loading...</div>;
   }
 
-  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   // eslint-disable-next-line no-unused-vars
   const notify = (message: string|null) => {
     setErrorMessage(message);
